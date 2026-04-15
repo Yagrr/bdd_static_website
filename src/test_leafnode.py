e6ca12ca1cd0a5500619ea46105576884a98070b
Yagrr <90156764+Yagrr@users.noreply.github.com>
2026-04-15 15:55:56 +0100

    chore(tests): improved test cases for leafnode and textnode

--- src/test_leafnode.py
+++ src/test_leafnode.py
@@ -3,15 +3,20 @@ import unittest
 from htmlnode import LeafNode
 
 
-class TestHTMLNode(unittest.TestCase):
-    def test_props_to_html(self):
+class TestLeafNode(unittest.TestCase):
+    def test_to_html(self):
         print("=== Testing LeafNode ===")
         node1 = LeafNode("p", "Testing leaf node")
         print(node1)
-        print(node1.to_html())
         node2 = LeafNode("a", "Leaf node with link here", {"href": "www.boot.dev"})
         print(node2)
-        print(node2.to_html())
+        print("Case: checking LeafNode correctly inherits")
+
+        value_assert1 = "<p>Testing leaf node</p>"
+        value_assert2 = '<a href="www.boot.dev">Leaf node with link here</a>'
+
+        self.assertEqual(node1.to_html(), value_assert1)
+        self.assertEqual(node2.to_html(), value_assert2)
 
 
 if __name__ == "__main__":
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -6,14 +6,19 @@ from textnode import TextNode, TextType
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
         print("=== Testing TextNode ===")
-        node = TextNode("This is a text node", TextType.BOLD)
-        node2 = TextNode("This is a text node", TextType.BOLD)
+        print("=== Case: text, bold, code, link ===")
+        node = TextNode("This is a text node", TextType.TEXT)
+        node_same = TextNode("This is a text node", TextType.TEXT)
+        node_bold = TextNode("This is a text node", TextType.BOLD)
         node3 = TextNode("`hello = 'this is some code'`", TextType.CODE)
         node4 = TextNode(
             "This should be wrapped as a link", TextType.LINK, "https://google.com"
         )
         node5 = TextNode("This should be wrapped as a link", TextType.LINK)
-        self.assertEqual(node, node2)
+
+        print("=== Case: equality between nodes ===")
+        self.assertEqual(node, node_same)
+        self.assertNotEqual(node, node_bold)
         self.assertNotEqual(node3, node4)
         self.assertNotEqual(node4, node5)
 
