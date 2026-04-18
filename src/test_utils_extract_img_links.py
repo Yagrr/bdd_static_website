05a7b74f3026a3cc55c64101f21b00531b841dd9
Yagrr <90156764+Yagrr@users.noreply.github.com>
2026-04-18 19:14:17 +0100

    feat(renderMD): add new capabilities for link and image extraction from raw markdown text (utils_extract_img_links.py). add associated tests

---
+++ src/test_utils_extract_img_links.py
@@ -0,0 +1,60 @@
+from utils_extract_img_links import extract_markdown_images, extract_markdown_links
+
+import unittest
+
+
+class TestMarkdownExtraction(unittest.TestCase):
+    def test_extract_markdown_images(self):
+        print("=== Testing markdown image extraction  ===")
+
+        print("Test case: text string with no images")
+        text1 = "This is a text with a link [to boot dev](https://www.boot.dev)"
+        result1 = extract_markdown_images(text1)
+        assert1 = []
+
+        self.assertEqual(result1, assert1)
+
+        print("Test case: text string with multiple images")
+        text2 = "This is an image of the Python logo ![image of the python logo](https://i.imgur.com/zjjcJKZ.png), and another ![image of the python logo 2](https://i.imgur.com/zjjcJKZ.png)"
+        result2 = extract_markdown_images(text2)
+        assert2 = [
+            ("image of the python logo", "https://i.imgur.com/zjjcJKZ.png"),
+            ("image of the python logo 2", "https://i.imgur.com/zjjcJKZ.png"),
+        ]
+        self.assertEqual(result2, assert2)
+
+        print("Test case: text string with both images and links")
+        text3 = "Here is a [link](https://www.boot.dev) image 1: ![image of a cat sitting](src/cat.png), image2: ![image of an orange cat](src/cat2.png)"
+        result3 = extract_markdown_images(text3)
+        assert3 = [
+            ("image of a cat sitting", "src/cat.png"),
+            ("image of an orange cat", "src/cat2.png"),
+        ]
+        self.assertEqual(result3, assert3)
+
+        return
+
+    def test_extract_markdown_links(self):
+        print("=== Testing markdown link extraction  ===")
+
+        print("Test case: text string with no links")
+        text_no_images = "image 1: ![image of a cat sitting](src/cat.png), image2: ![image of an orange cat](src/cat2.png)"
+        result1 = extract_markdown_links(text_no_images)
+        assert1 = []
+        self.assertEqual(result1, assert1)
+
+        print("Test case: text string with multiple links")
+        text1 = "This is a text with a link [to boot dev](https://www.boot.dev). Here is a second link to [Google](https://www.google.com)"
+        result1 = extract_markdown_links(text1)
+        assert1 = [
+            ("to boot dev", "https://www.boot.dev"),
+            ("Google", "https://www.google.com"),
+        ]
+
+        print("Test case: text string with both images and links")
+        text2 = "Here is a [link](https://www.boot.dev) image 1: ![image of a cat sitting](src/cat.png), image2: ![image of an orange cat](src/cat2.png)"
+        result2 = extract_markdown_links(text2)
+        assert2 = [("link", "https://www.boot.dev")]
+        self.assertEqual(result2, assert2)
+
+        return
---
+++ src/utils_extract_img_links.py
@@ -0,0 +1,43 @@
+import re
+
+
+def extract_markdown_images(text_md: str) -> list[tuple]:
+    """From a string input of raw markdown text, extract all substrings
+    matching the markdown images pattern into a list, return a list of tuples
+    where each tuple contains the alt text and associated URL of any markdown
+    images contained within the string.
+
+    Example:
+    ```
+    text = "This is some text followed by an image. [image of a cat](/assets/cat.png) Here's another one. [image of an orange cat](/assets/cat2.png)"
+    print(extract_markdown_links(text))
+    # [("image of a cat", "/assets/cat.png"), ("image of an orange cat", "/assets/cat2.png")]
+    ```
+    """
+    # Pattern: "![*](*)"
+
+    pattern_img = r'!\[([a-zA-Z0-9."\'\-\_ ]+)\]\(([a-zA-Z0-9.:/@\'"\-\_]+)(?=\))'
+    matches = re.findall(pattern_img, text_md)
+
+    return matches
+
+
+def extract_markdown_links(text_md: str) -> list[tuple]:
+    """From a string input of raw markdown text, extract all substrings
+    matching the markdown link pattern into a list, then return a list of tuples where
+    each tuple contains the anchor text and associated URL contained within the
+    input string.
+
+    Example:
+    ```
+    text = "This is a text with a link to [a website](https://www.boot.dev)
+    print(extract_markdown_links(text))
+    # [("a website", "https://www.boot.dev")]
+    ```
+    """
+
+    # Pattern: "[*](https://*)"
+    pattern_link = r'(?<!!)\[([a-zA-Z0-9."\'\-\_ ]+)\]\(([a-zA-Z0-9.:/@\'"\-\_]+)(?=\))'
+    matches = re.findall(pattern_link, text_md)
+
+    return matches
