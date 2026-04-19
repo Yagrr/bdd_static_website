63a6c6daea2d40b54c57490f583531957cc52a45
Yagrr <90156764+Yagrr@users.noreply.github.com>
2026-04-19 15:59:10 +0100

    refactor!(renderMD): migrate textnode_to_htmlnode function to textnode.py file. Add type hints to split_nodes_delimiter function in utils_inline_markdown.

--- src/utils_inline_markdown.py
+++ src/utils_inline_markdown.py
@@ -1,32 +1,11 @@
 from textnode import TextNode, TextType
-from htmlnode import LeafNode
 
 import re
 
 
-def textnode_to_htmlnode(text_node: TextNode) -> LeafNode:
-    """Returns a new HTMLNode based on the text_type attribute of the input
-    TextNode.
-
-    TODO: add logic for ParentNode creation. Current implementation only
-    creates a new LeafNode
-    """
-    match text_node.text_type:
-        case TextType.TEXT:
-            return LeafNode(None, text_node.text)
-        case TextType.BOLD:
-            return LeafNode("b", text_node.text)
-        case TextType.ITALIC:
-            return LeafNode("i", text_node.text)
-        case TextType.CODE:
-            return LeafNode("code", text_node.text)
-        case TextType.LINK:
-            return LeafNode("a", text_node.text, {"href": text_node.url})
-        case TextType.IMG:
-            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
-
-
-def split_nodes_delimiter(nodes_old, delimiter, text_type) -> list[TextNode]:
+def split_nodes_delimiter(
+    nodes_old: list[TextNode], delimiter, text_type
+) -> list[TextNode]:
     """From an input list of TextNodes of TextType.TEXT, splits each
     TextNode.text by a provided delimiter into a new list of new TextNodes with the
     input text_type. Can handle unclosed delimiters.
